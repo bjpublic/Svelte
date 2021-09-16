09e848d3632688a6adafe51c8a793db56cc5722c
BJPUBLIC <bjpublic@bjpublic.co.kr>
2021-09-16 11:04:58 +0900

    Delete ch04 directory

--- ch04/code14/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  let html = '<p>내용을 입력해주세요...</p>';
-</script>
-
-<div contenteditable="true" bind:innerHTML={html}></div>
-
-<pre>{html}</pre>
--- ch04/code15-16/increment.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  export let count
-  
-  const increNum = () => count = Number(count) + 1;
-</script>
-
-<button on:click={increNum}>Increment</button>
