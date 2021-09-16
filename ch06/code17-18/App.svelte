fae0ddd37548eb4d6c8e636a02ed12cc8511502d
BJPUBLIC <bjpublic@bjpublic.co.kr>
2021-09-16 11:05:27 +0900

    Delete ch06 directory

--- ch06/code17-18/App.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  import writer from "./writer.js";
-</script>
-
-<h1 use:writer>타이핑하는 효과입니다.</h1>
--- ch06/code19-20/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import writer from "./writer.js";
-  let setSpeed = 100;
-</script>
-
-<h1 use:writer={setSpeed}>타이핑하는 효과입니다.</h1>
-
-<button on:click={() => setSpeed = 100} >속도 100</button>
-<button on:click={() => setSpeed = 1000}>속도 1000</button>
