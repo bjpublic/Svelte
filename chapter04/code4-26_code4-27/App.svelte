1b65d9c697f39efa56af95d4f99e1eb17c1047af
freeseamew <freeseamew@gmail.com>
2021-11-12 17:46:16 +0900

    저자 교정 중 내용수정

---
+++ chapter04/code4-26_code4-27/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { tick } from "svelte";
+  let a = 1;
+  let b = 2;
+  let sumElement;
+
+  async function handleUpdate() {
+    a = 2;
+    b = 5;
+
+    // tick을 이용해 변경된 상태값이 돔에 적용되기를 기다림
+		await tick();
+
+    // Logs 2 + 5 = 7
+		console.log(`${a}, ${b}`);
+    console.log(sumElement.textContent);
+  }
+</script>
+
+<div class="sum" bind:this={sumElement}>
+  {a} + {b} = {a + b}
+</div>
+<button on:click={handleUpdate}>
+  숫자 업데이트
+</button>
