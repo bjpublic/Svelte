d4126f3359dbdf6b5704fd370434f88ddf7f1547
Muang <33769671+hndada@users.noreply.github.com>
2022-12-19 18:36:06 +0900

    오타 수정
    곡 -> 꼭

--- chapter03/code3-1_code3-7/App.svelte
+++ chapter03/code3-1_code3-7/App.svelte
@@ -4,7 +4,7 @@ import Content from './content.svelte';
 import Footer from './footer.svelte';
 </script>
 
-<Header /> <!-- 컴포넌트는 곡 대문자로 시작해야함-->
+<Header /> <!-- 컴포넌트는 꼭 대문자로 시작해야함-->
 <Content />
 <Content />
 <Footer />
